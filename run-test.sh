04042828a8598293324a33b5b29447c93197c5f2
yushaku <levanson180200@gmail.com>
2024-08-20 16:03:58 +0700

    🧪 (test): Add grafana + k6 for testing

---
+++ run-test.sh
@@ -0,0 +1,6 @@
+docker compose up -d influxdb grafana
+docker compose run k6 run /test/stress.test.js
+
+echo "--------------------------------------------------------------------------------------"
+echo "Load testing with Grafana dashboard http://localhost:3000/d/k6/k6-load-testing-results"
+echo "--------------------------------------------------------------------------------------"
---
+++ test/stress.test.js
@@ -0,0 +1,45 @@
+import http from 'k6/http'
+import { check, sleep } from 'k6'
+import { Counter } from 'k6/metrics'
+
+export const failedRequests = new Counter('failed_requests')
+
+const BASE_URL = 'https://dev.api.anonymaus.fyi'
+//
+//NOTE: Number of virtual users
+let target_vus = 50
+target_vus = 200
+// target_vus = 1000
+
+// NOTE: replace with your access token
+const access_token = ''
+
+export let options = {
+  stages: [
+    { duration: '30s', target: target_vus },
+    {
+      duration: '30s',
+      target: target_vus,
+      thresholds: {
+        http_req_duration: ['p(95)<2000'], // 95% of requests must complete below 2000ms
+        failed_requests: ['count<10'], // Allow fewer than 10 failed requests
+      },
+    },
+    { duration: '20s', target: 0 },
+  ],
+}
+
+export default function () {
+  const response = http.get(`${BASE_URL}/post?page=1&perPage=20&type=hotest`, {
+    headers: {
+      Accepts: 'application/json',
+      authorization: `Bearer ${access_token}`,
+    },
+  })
+  const success = check(response, {
+    'GET request is successful': (r) => r.status === 200,
+  })
+
+  if (!success) failedRequests.add(1)
+  sleep(0.3)
+}
